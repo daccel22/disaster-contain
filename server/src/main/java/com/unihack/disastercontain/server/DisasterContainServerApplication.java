365457a54708dd6b4a659b8e8e91038c5e1fce76
yc-22 <yi.chen@mykaarma.com>
2021-08-06 15:38:34 -0700

    Initial publish

---
+++ server/src/main/java/com/unihack/disastercontain/server/DisasterContainServerApplication.java
@@ -0,0 +1,13 @@
+package com.unihack.disastercontain.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterContainServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterContainServerApplication.class, args);
+	}
+
+}
