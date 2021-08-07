c8539eb31b7aec7d749495e8bfa7900e566f30d8
hoyi <hoyi@umich.edu>
2021-08-07 12:38:12 +0800

    排除数据库

--- server/src/main/java/com/unihack/disastercontain/server/DisasterContainServerApplication.java
+++ server/src/main/java/com/unihack/disastercontain/server/DisasterContainServerApplication.java
@@ -2,8 +2,9 @@ package com.unihack.disastercontain.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class DisasterContainServerApplication {
 
 	public static void main(String[] args) {
